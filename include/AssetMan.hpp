b66bae410be3217fee4465b51646bb51599be4d1
Ryuzaki <ufrshubham@gmail.com>
2020-03-21 21:12:48 +0530

    Added Asset Manager

---
+++ include/AssetMan.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <map>
+#include <memory>
+#include <string>
+
+#include <SFML/Graphics/Texture.hpp>
+#include <SFML/Graphics/Font.hpp>
+
+namespace Engine
+{
+class AssetMan
+{
+private:
+    std::map<int, std::unique_ptr<sf::Texture>> m_textures;
+    std::map<int, std::unique_ptr<sf::Font>> m_fonts;
+
+public:
+    AssetMan();
+    ~AssetMan();
+
+    void AddTexture(int id, const std::string &filePath, bool wantRepeated = false);
+    void AddFont(int id, const std::string &filePath);
+
+    const sf::Texture &GetTexture(int id) const;
+    const sf::Font &GetFont(int id) const;
+};
+} // namespace Engine
---
+++ src/AssetMan.cpp
@@ -0,0 +1,40 @@
+#include "AssetMan.hpp"
+
+Engine::AssetMan::AssetMan()
+{
+}
+
+Engine::AssetMan::~AssetMan()
+{
+}
+
+void Engine::AssetMan::AddTexture(int id, const std::string &filePath, bool wantRepeated)
+{
+    auto texture = std::make_unique<sf::Texture>();
+
+    if(texture->loadFromFile(filePath))
+    {
+        texture->setRepeated(wantRepeated);
+        m_textures[id] = std::move(texture);
+    }
+}
+
+void Engine::AssetMan::AddFont(int id, const std::string &filePath)
+{
+    auto font = std::make_unique<sf::Font>();
+
+    if(font->loadFromFile(filePath))
+    {
+        m_fonts[id] = std::move(font);
+    }
+}
+
+const sf::Texture &Engine::AssetMan::GetTexture(int id) const
+{
+    return *(m_textures.at(id).get());
+}
+
+const sf::Font &Engine::AssetMan::GetFont(int id) const
+{
+    return *(m_fonts.at(id).get());
+}
